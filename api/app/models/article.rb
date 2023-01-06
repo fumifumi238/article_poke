e1a43e3ca97331b9a9ffebcf7fb30a5fdc1bce4f
epthalite <fmy628.1998@icloud.com>
2023-01-06 17:26:13 +0900

    name イカサマダイス いかさまダイス

--- api/app/models/article.rb
+++ api/app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :parties,dependent: :destroy
   VALID_URL_REGEX = /\Ahttps?:\/\/[\w\/:%#\$&\?\(\)~\.=\+\-]+\z/
-  validates :url,presence: true,format: { with: VALID_URL_REGEX },length: { maximum: 100 },uniqueness: { case_sensitive: false }
+  validates :url,presence: true,format: { with: VALID_URL_REGEX },uniqueness: { case_sensitive: false }
   validates :season,presence: true, numericality: {greater_than: 0,less_than: 100}
   validates :series,presence: true, numericality: {greater_than: 0,less_than: 100}
   validates :rate, allow_nil: true,numericality: {greater_than: 100,less_than: 10000}
