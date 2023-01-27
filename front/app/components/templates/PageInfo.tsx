925c37df1253736730acfc3c379c07098820d65c
epthalite <fmy628.1998@icloud.com>
2023-01-27 19:44:01 +0900

    finish index page

---
+++ front/app/components/templates/PageInfo.tsx
@@ -0,0 +1,54 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Image from "next/image";
+import NextLink from "next/link";
+import { useRouter } from "next/router";
+import { ReactNode } from "react";
+
+type PageInfo = {
+  value: string;
+  url: string;
+  imageSrc: string;
+  discriptions: string[];
+  icon: ReactNode;
+};
+
+const PageInfo = ({ value, url, imageSrc, discriptions, icon }: PageInfo) => {
+  const router = useRouter();
+
+  const onClickValue = () => {
+    router.push(url);
+  };
+
+  return (
+    <Box sx={{ margin: 1 }}>
+      <Box
+        sx={{
+          display: "flex",
+          borderBottom: "#212F3D",
+          alignItems: "center",
+        }}>
+        <Button sx={{ color: "darkgrey" }} onClick={onClickValue}>
+          <Box sx={{ marginRight: 2 }}>{icon}</Box>
+          <Typography variant="h4">{value}</Typography>
+        </Button>
+      </Box>
+      <Box sx={{ margin: 2, display: "flex", justifyContent: "center" }}>
+        <Image src={imageSrc} alt={`${value} 例`} width={400} height={400} />
+      </Box>
+      <Box sx={{ borderTop: "solid grey", margin: 3, paddingTop: 2 }}>
+        {discriptions.map((discription) => (
+          <Typography sx={{ fontWeight: 800 }}>{discription}</Typography>
+        ))}
+      </Box>
+      <Box sx={{ textAlign: "right" }}>
+        <NextLink href={url}>
+          <Button>{value}ページを見る</Button>
+        </NextLink>
+      </Box>
+    </Box>
+  );
+};
+
+export default PageInfo;
--- front/app/pages/index.tsx
+++ front/app/pages/index.tsx
@@ -5,31 +5,88 @@ import { NextPage } from "next";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import Rule from "../components/templates/Rule";
+import PageInfo from "../components/templates/PageInfo";
+import ArticleIcon from "@mui/icons-material/Article";
+import CreateIcon from "@mui/icons-material/Create";
+
+import PersonSearchIcon from "@mui/icons-material/PersonSearch";
+import CatchingPokemonIcon from "@mui/icons-material/CatchingPokemon";
+import ErrorIcon from "@mui/icons-material/Error";
+import TroubleshootIcon from "@mui/icons-material/Troubleshoot";
 
 type Format = "single" | "double";
 
 const Home: NextPage = () => {
   const router = useRouter();
+  const statsDiscriptions = [
+    "・ポケモン名を入力すると、そのポケモンが使われている記事の努力値、持ち物、性格の一覧が見れます。",
+  ];
+  const formDiscriptions = [
+    "・構築記事の投稿ができます。努力値は必須ではないですが、入力していただければ、記事一覧のページや型検索から努力値がわかります。",
+    "・性格はチェックボタン(☑)を押しても変えれます。",
+    "・Twitter idを入力するとシーズンごとの成績が見れます。",
+  ];
 
-  const redirectToArticle = (format: Format = "single") => {
-    router.push(`/article?format=${format}?series=1`);
-  };
+  const articleDiscriptions = [
+    "・タイトル名をクリックすると、構築記事に飛べます。",
+    "・詳細をクリックすると、技や努力値が見れます。",
+    "・使用率をクリックすると、ポケモンやテラスタルの採用率が見れます。",
+    "・表示設定から順位、フォーマット(シングルかダブル)、シーズンの変更ができます。",
+    "・入力欄にポケモン名を区切って入れると、そのポケモンが入っている構築が出ます。",
+    "・トレーナー名をクリックすると、その人のシーズンごとの成績が見れます。",
+  ];
 
   return (
     <>
-      <div>
+      <Box>
         {/* <Rule series="2" /> */}
         <Rule series="1" />
-        <Box>
-          <Typography>機能一覧</Typography>
+        <Box
+          sx={{
+            border: 1,
+            width: "100%",
+            maxWidth: 600,
+            margin: "0 auto",
+            display: "flex",
+            justifyContent: "center",
+            borderRadius: "5px",
+            marginY: 2,
+          }}>
           <Box>
-            <Typography>型検索</Typography>
-            <Typography>
-              ポケモン名を入力することで、そのポケモンの努力値と持ち物、性格の一覧が見れます。
+            <Typography
+              variant="h3"
+              sx={{
+                bgcolor: "#689BCD",
+                color: "white",
+                fontFamily: "fantasy",
+                paddingLeft: 2,
+              }}>
+              Pages
             </Typography>
+            <PageInfo
+              icon={<ArticleIcon sx={{ height: 40, width: 40 }} />}
+              value="構築記事一覧"
+              url="/article"
+              imageSrc="/image/pageInfo/article.jpg"
+              discriptions={articleDiscriptions}
+            />
+            <PageInfo
+              icon={<CreateIcon sx={{ height: 40, width: 40 }} />}
+              value="構築記事投稿"
+              url="/form"
+              imageSrc="/image/pageInfo/form.png"
+              discriptions={formDiscriptions}
+            />
+            <PageInfo
+              icon={<TroubleshootIcon sx={{ height: 40, width: 40 }} />}
+              value="型検索"
+              url="/stats"
+              imageSrc="/image/pageInfo/stats.png"
+              discriptions={statsDiscriptions}
+            />
           </Box>
         </Box>
-      </div>
+      </Box>
     </>
   );
 };
